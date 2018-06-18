90d9038273bdfd423c70a65626a1431aec6ca995
Jackie Liu <jinke.liu@autodesk.com>
2018-06-18 10:19:26 +0800

    Add on_throttle

--- app.js
+++ app.js
@@ -11,7 +11,23 @@ const PORT = process.env.PORT || 5000;
 
 app.use(responseTime());
 
-const requestThrottle = throttle({ burst: 10, rate: "5/s" });
+const requestThrottle = throttle({
+  burst: 10,
+  rate: "5/s",
+  on_throttled: function(req, res, next, bucket) {
+    // Possible course of actions:
+    // 1) Log request
+    // 2) Add client ip address to a ban list
+    // 3) Send back more information
+    console.log("Exceeded rate limit.");
+    res.set("X-Rate-Limit-Limit", 5);
+    res.set("X-Rate-Limit-Remaining", 0);
+    // bucket.etime = expiration time in Unix epoch ms, only available
+    // for fixed time windows
+    res.set("X-Rate-Limit-Reset", bucket.etime);
+    res.status(503).send("System overloaded, try again at a later time.");
+  }
+});
 
 app.get("/fib/:n", requestThrottle, (req, res) => {
   console.log(`started fib${req.params.n}: ${new Date().toISOString()}`);
